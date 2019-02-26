0873e9143f8a0aa04e82f8855488a8131179343c
曲振富 <quzhenfu@jingdata.com>
2019-02-26 11:47:03 +0800

    新增git自动上传的脚本

--- normal/AutoGitPush.py
+++ normal/AutoGitPush.py
@@ -10,6 +10,7 @@ def test(projects=[], basePath='/Users/bjqxdn0702/IdeaProjects'):
     # Log.v(val)
     # subprocess.call('cd ~/IdeaProjects/jingdata-paas-metadata/ && git push new --all', shell=True)
     for projectName in projects:
+        Log.v("start %s " % projectName)
         subprocess.check_call('git pull', shell=True, cwd=basePath + "/" + projectName)
         subprocess.check_call('git push new --all ', shell=True, cwd=basePath + "/" + projectName)
 
@@ -17,7 +18,8 @@ def test(projects=[], basePath='/Users/bjqxdn0702/IdeaProjects'):
 def main():
     test(
         ['jingdata-paas-metadata', 'jingdata-paas-message', 'jingdata-paas-search', 'jingdata-api-gateway', 'jingdata-paas-org',
-         'jingdata-paas-auth', 'jingdata-paas-datarights', 'jingdata-paas-log-collection', 'jingdata-saas-investment'])
+         'jingdata-paas-auth', 'jingdata-paas-datarights', 'jingdata-paas-log-collection', 'jingdata-saas-investment', 'jingdata-paas-log',
+         'jingdata-paas-common', 'jingdata-paas-common-extend', 'jingdata-paas-common-strength', 'jingdata-paas-common-basic'])
     test(['jingdata-admin-web'], '/Users/bjqxdn0702/Documents/36kr')
     Log.v("全部执行完成")
 
