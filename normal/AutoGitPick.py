abf6499093881d9e0ea619a6c72422c35bd0e4e1
曲振富 <quzhenfu@jingdata.com>
2019-03-11 16:28:59 +0800

    新增git自动上传的脚本

---
+++ normal/AutoGitPick.py
@@ -0,0 +1,47 @@
+from utils.Log import Log
+import os
+import subprocess
+import re
+
+
+def test(projectName=None, basePath='/Users/bjqxdn0702/IdeaProjects', branchs=[], pickVersion=None):
+    # val = os.system("cd ~/IdeaProjects/jingdata-paas-metadata/")
+    # val = os.system('git pull')
+    # val = os.system('git push new --all')
+    # Log.v(val)
+    # subprocess.call('cd ~/IdeaProjects/jingdata-paas-metadata/ && git push new --all', shell=True)
+    if (branchs == None or pickVersion == None):
+        return
+
+    Log.v("start %s " % projectName)
+    for branch in branchs:
+        path = basePath + "/" + projectName
+        excute(path, 'git pull ')
+        checkResult = excute(path, 'git checkout ' + branch)
+        if (re.match('.* branch is up to date with \'origin/%s\'.*' % branch, checkResult)):
+            Log.v(checkResult)
+        else:
+            raise ValueError("checkout error %s", checkResult)
+        cherryPickResult = excute(path, 'git cherry-pick ' + pickVersion)
+        Log.v(cherryPickResult)
+        # subprocess.check_call('git pull ', shell=True, cwd=basePath + "/" + projectName)
+        # subprocess.check_call('git checkout ' + branch, shell=True, cwd=basePath + "/" + projectName)
+        # subprocess.check_call('git cherry-pick ' + pickVersion, shell=True, cwd=basePath + "/" + projectName)
+        # subprocess.check_call('git push new --all ', shell=True, cwd=basePath + "/" + projectName)
+
+
+def excute(cwd, shell):
+    p = subprocess.run(shell, cwd=cwd, shell=True, stdout=subprocess.PIPE, check=True)
+    # p = subprocess.Popen(shell, cwd=cwd, shell=True, stdout=subprocess.PIPE)
+    # p.wait()
+    # out, error = p.communicate()
+    return p.stdout.decode()
+
+
+def main():
+    test('jingdata-saas-investment', branchs=['lvdi'], pickVersion='0723695')
+    Log.v("全部执行完成")
+
+
+if __name__ == '__main__':
+    main()
--- normal/AutoGitPush.py
+++ normal/AutoGitPush.py
@@ -9,6 +9,7 @@ def test(projects=[], basePath='/Users/bjqxdn0702/IdeaProjects'):
     # val = os.system('git push new --all')
     # Log.v(val)
     # subprocess.call('cd ~/IdeaProjects/jingdata-paas-metadata/ && git push new --all', shell=True)
+    # subprocess.call('cd ~/IdeaProjects/jingdata-paas-metadata/ && git push new --all', shell=True)
     for projectName in projects:
         Log.v("start %s " % projectName)
         subprocess.check_call('git pull', shell=True, cwd=basePath + "/" + projectName)
@@ -16,8 +17,9 @@ def test(projects=[], basePath='/Users/bjqxdn0702/IdeaProjects'):
 
 
 def main():
+    #'jingdata-api-gateway',
     test(
-        ['jingdata-paas-metadata', 'jingdata-paas-message', 'jingdata-paas-search', 'jingdata-api-gateway', 'jingdata-paas-org',
+        ['jingdata-paas-metadata', 'jingdata-paas-message', 'jingdata-paas-search',  'jingdata-paas-org',
          'jingdata-paas-auth', 'jingdata-paas-datarights', 'jingdata-paas-log-collection', 'jingdata-saas-investment', 'jingdata-paas-log',
          'jingdata-paas-common', 'jingdata-paas-common-extend', 'jingdata-paas-common-strength', 'jingdata-paas-common-basic'])
     test(['jingdata-admin-web'], '/Users/bjqxdn0702/Documents/36kr')
